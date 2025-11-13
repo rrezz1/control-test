2946ed7ed62a5482e2566ab9204dfa6a5b7db014
Rrezart Buzuku <rrezbuzuku@gmail.com>
2025-11-13 18:45:18 +0100

    Delete components directory

--- components/ServiceCard.js
+++
@@ -1,9 +0,0 @@
-export default function ServiceCard({ title, description, icon, language }) {
-  return (
-    <div className="service-card">
-      <div className="service-icon">{icon}</div>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
